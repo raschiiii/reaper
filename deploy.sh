3993ac04122d86f668300d74caafa0165310e018
Jakob <e11809618@student.tuwien.ac.at>
2021-04-20 09:42:22 +0200

    refactor

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,19 @@ remote=neuromancer:/var/www/vaporware.ga/reaper/
 zip -ur $build assets/ 
 
 zip -ur $build index.html
-zip -ur $build src/*.js 
-zip -ur $build src/terrain/ src/physics/ src/particles/ 
-zip -ur $build src/components/ src/engine/ src/collision/  
+zip -ur $build js/*.js 
+zip -ur $build js/terrain/ src/physics/ src/particles/ 
+zip -ur $build js/components/ src/engine/ src/collision/  
 
 zip -ur $build LICENSE
 
-# zip -ur $build src/three/build/three.module.js 
-# zip -ur $build src/three/examples/jsm/libs/stats.module.js 
-# zip -ur $build src/three/examples/jsm/controls/OrbitControls.js
-# zip -ur $build src/three/examples/jsm/loaders/GLTFLoader.js 
-# zip -ur $build src/three/examples/jsm/postprocessing/* 
-# zip -ur $build src/three/examples/jsm/shaders/* 
+# zip -ur $build js/three/build/three.module.js 
+# zip -ur $build js/three/examples/jsm/libs/stats.module.js 
+# zip -ur $build js/three/examples/jsm/controls/OrbitControls.js
+# zip -ur $build js/three/examples/jsm/loaders/GLTFLoader.js 
+# zip -ur $build js/three/examples/jsm/postprocessing/* 
+# zip -ur $build js/three/examples/jsm/shaders/* 
 
 scp $build $remote
 
 rm -f $build
-
-
