27242ebf0124b50e878750fe432e2f2dedfea1fd
Jakob <jakob.g.maier@gmail.com>
2021-05-02 15:06:00 +0200

    fixed bug

--- src/components/components.js
+++ src/components/components.js
@@ -34,7 +34,7 @@ export class Explosive extends Component {
         this.gameObject.subscribe("collision", () => {
             if (!hasExploded) {
                 hasExploded = true;
-                this.gameObject.lifetime = 5;
+                this.gameObject.lifetime = 15;
 
                 this.gameObject.removeComponent(AABB);
                 this.gameObject.removeComponent(Physics);
